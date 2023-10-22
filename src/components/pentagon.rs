16dc5806e3388ce39075c5e231e39d7eafba1197
Márió Hegyi <mario.hegyi93@gmail.com>
2023-10-22 16:03:04 +0200

    Depth buffer

--- src/components/pentagon.rs
+++ src/components/pentagon.rs
@@ -193,7 +193,13 @@ impl Pentagon {
                 cull_mode: Some(wgpu::Face::Back),
                 ..Default::default()
             },
-            depth_stencil: None,
+            depth_stencil: Some(wgpu::DepthStencilState {
+                format: wgpu::TextureFormat::Depth32Float,
+                depth_write_enabled: true,
+                depth_compare: wgpu::CompareFunction::Less,
+                stencil: wgpu::StencilState::default(),
+                bias: wgpu::DepthBiasState::default(),
+            }),
             multisample: wgpu::MultisampleState::default(),
             multiview: None,
         });
--- src/main.rs
+++ src/main.rs
@@ -158,6 +158,35 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
         view_formats: &[],
     });
 
+    let depth_texture = device.create_texture(&wgpu::TextureDescriptor {
+        label: Some("Depth texture"),
+        size: wgpu::Extent3d {
+            width: size.width,
+            height: size.height,
+            ..Default::default()
+        },
+        mip_level_count: 1,
+        sample_count: 1,
+        dimension: wgpu::TextureDimension::D2,
+        format: wgpu::TextureFormat::Depth32Float,
+        usage: wgpu::TextureUsages::RENDER_ATTACHMENT | wgpu::TextureUsages::TEXTURE_BINDING,
+        view_formats: &[],
+    });
+
+    let depth_view = depth_texture.create_view(&wgpu::TextureViewDescriptor::default());
+    // let depth_sampler = device.create_sampler(&wgpu::SamplerDescriptor {
+    //     address_mode_u: wgpu::AddressMode::ClampToEdge,
+    //     address_mode_v: wgpu::AddressMode::ClampToEdge,
+    //     address_mode_w: wgpu::AddressMode::ClampToEdge,
+    //     mag_filter: wgpu::FilterMode::Linear,
+    //     min_filter: wgpu::FilterMode::Linear,
+    //     mipmap_filter: wgpu::FilterMode::Nearest,
+    //     compare: Some(wgpu::CompareFunction::LessEqual), // 5.
+    //     lod_min_clamp: 0.0,
+    //     lod_max_clamp: 100.0,
+    //     ..Default::default()
+    // });
+
     let mut camera = Camera::default();
 
     let mut pentagon_model = Pentagon::new(
@@ -274,7 +303,14 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
                                 store: true,
                             },
                         })],
-                        depth_stencil_attachment: None,
+                        depth_stencil_attachment: Some(wgpu::RenderPassDepthStencilAttachment {
+                            view: &depth_view,
+                            depth_ops: Some(wgpu::Operations {
+                                load: wgpu::LoadOp::Clear(1.0),
+                                store: true,
+                            }),
+                            stencil_ops: None,
+                        }),
                     });
                     pentagon_model.render(&mut rpass);
                 }
